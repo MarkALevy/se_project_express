fa4f3c27d8c69de52122f4fc0456ce36e4fd4647
Mark Levy <levymark@gmail.com>
2024-10-11 18:02:42 +0300

    Create the project structure

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const { PORT = 3001 } = process.env;
+
+mongoose.set('strictQuery', true);
+
 mongoose
   .connect('mongodb://127.0.0.1:27017/wtwr_db')
   .then(() => {
